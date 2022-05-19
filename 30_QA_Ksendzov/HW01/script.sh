c58c723621b485eab01d6bf58a0aba82547d86bc
alexmyt <alexmyt1@yandex.ru>
2022-05-19 17:09:31 +0300

    Optimize steps with 'touch' command

--- 30_QA_Ksendzov/HW01/script.sh
+++ 30_QA_Ksendzov/HW01/script.sh
@@ -2,7 +2,7 @@ find ./dir1/* -delete
 cd dir1
 mkdir dir2 dir3 dir4
 cd dir2
-touch file1.txt && touch file2.txt && echo "any text" > file3.txt && touch file4.json && touch file5.json
+touch file1.txt file2.txt file3.txt file4.json file5.json
 mkdir dir5 dir6 dir7
 ls -l
 mv file1.txt file2.txt dir5
